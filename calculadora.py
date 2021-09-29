81e89ae4c594cd98a7893248e97149909e6f2f2b
maiconqueias <77929488+maiconqueias@users.noreply.github.com>
2021-09-29 17:18:04 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+print('ola mundo' , 'essa é uma calculadora que foi criada por miqueias!')
+
+
+nome = input('digite seu nome:')
+print('olá! muto prazer em te conhecer' , nome )
+
+#parte da calculadora
+
+n1 = int(input('digite um numero:'))
+n2 = int(input('digite outro numero:'))
+s = n1 + n2
+print('a soma desses numeros dão:' , s)
+print('vamos complicar mais?')
+sim = input("se quiser fale sim, se não aperte ENTER:")
+if sim:
+    print('que bom! então vamos começar:')
+    n3 = int(input('digite um numero:'))
+    n4 = int(input('digite outro numero:'))
+    resposta = n3 * n4
+    print('O primeiro número que voce digitou multiplicado por outro número que voce digitou dá:' , resposta)
+else:
+    print('QUE PENA FICA PARA PROXIMA!')
+
+
+
+
+
+
+
+  
