49cd99abaf46657aeee18bdd7827030bbfc343d9
maiconqueias <77929488+maiconqueias@users.noreply.github.com>
2021-10-03 09:52:15 -0300

    Add files via upload

---
+++ projeto 2.py
@@ -0,0 +1,37 @@
+nome = input ("digite seu nome:")
+print("ola!" ,nome)
+perg1 = int(input( "você tem quantos anos?"))
+print("olha você tem" , 14, "anos")
+ 
+if perg1 > 18 :
+   print("você é de maior!")
+else:
+   print("você não é de maior")
+   
+pais= input("você é brasileiro?")
+
+if pais == "sim":
+    
+   print("você é brasileiro aue legal!")
+   
+else:
+   print("você não é brasileiro que pena")
+   
+pais2 = input("você é de qual país então?")
+
+print("que legal!")
+
+print("vamos jogar um jogo?")
+jogo = int(input("qual é a raiz quadrada de 144?"))
+
+if jogo == "144":
+    print("você acertou parabéns!")
+    
+else:
+    print(" que pena você errou!")
+    
+
+    
+
+
+ 
